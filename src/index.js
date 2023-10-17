fdfe509f90a43f988dbef01f1ace230b8ec702ec
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-10-17 19:14:20 +0700

    install prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
-const morgan = require('morgan');
-var path = require('path');
+const express = require("express");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
+var path = require("path");
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
+const route = require("./routes");
 
 //Config thư mục public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Middleware, display post formData
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
@@ -23,10 +23,10 @@ app.use(express.json());
 // app.use(morgan("combined"));
 
 //Template Engine
-app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
-app.set('view engine', 'hbs');
+app.engine("hbs", engine({ extname: ".hbs", defaultLayout: "main" }));
+app.set("view engine", "hbs");
 //Config thư mục views
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set("views", path.join(__dirname, "resources/views"));
 
 //Router init
 route(app);
@@ -34,5 +34,5 @@ route(app);
 var array = { a: 1, b: 2 };
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });

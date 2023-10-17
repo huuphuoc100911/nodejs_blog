ef5bae1cbcd2a7c6c3401ac151adc27928108b0f
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-10-17 21:52:09 +0700

    show detail course

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,16 @@
+const Course = require('../models/Course');
+const { mongooseToObject } = require('../../utils/mongose');
+class CourseController {
+    //Get /courses/:slud
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => {
+                res.render('courses/show', {
+                    course: mongooseToObject(course),
+                });
+            })
+            .catch(next);
+    }
+}
+
+module.exports = new CourseController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const Course = new Schema({
     name: { type: String, maxLength: 255 },
+    slug: { type: String, maxLength: 100 },
     description: { type: String, maxLength: 600 },
     image: { type: String, maxLength: 255 },
     createAt: { type: Date, default: Date.now },

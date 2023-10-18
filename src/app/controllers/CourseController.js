a97233eb7e1f016a21a25933b8e610d8104a7ddd
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-10-18 23:59:31 +0700

    check all select box

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -65,6 +65,20 @@ class CourseController {
             .then(() => res.redirect('back'))
             .catch(next);
     }
+
+    //[POST] /courses/handle-form-actions
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is invalided!!' });
+                break;
+        }
+    }
 }
 
 module.exports = new CourseController();
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,7 @@ const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/handle-form-actions', courseController.handleFormActions);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
 router.patch('/:id/restore', courseController.restore);

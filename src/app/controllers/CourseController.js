bb1cae9baab73373ba8a928cb870217f8ae08c72
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-10-18 14:09:52 +0700

    crud courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
 const { mongooseToObject } = require('../../utils/mongose');
+const { storedCourses } = require('./MeController');
 class CourseController {
     //Get /courses/:slud
     show(req, res, next) {
@@ -9,6 +10,48 @@ class CourseController {
                     course: mongooseToObject(course),
                 });
             })
+            .catch(next);
+    }
+    //Get /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    //Post courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hq720.jpg`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/'))
+            .catch((error) => {});
+    }
+
+    //Get /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then((course) =>
+                res.render('courses/edit', {
+                    course: mongooseToObject(course),
+                }),
+            )
+            .catch(next);
+    }
+
+    //PUT /courses/:id/update
+    update(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hq720.jpg`;
+        Course.updateOne({ _id: req.params.id }, formData)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
+
+    // Delete /courses/:id/delete
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
             .catch(next);
     }
 }
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 
-const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    slug: { type: String, maxLength: 100 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        image: { type: String, maxLength: 255 },
+        videoId: { type: String, required: true },
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
